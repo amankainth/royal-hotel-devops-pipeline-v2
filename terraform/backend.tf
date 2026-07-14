33fb5d2585a2a41e7952ee42c2ea77beb9b54543
amankainth <aman.kainth@gmail.com>
2026-07-14 12:12:22 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "royal-hotel-tf-state-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "royal-hotel-tf-locks"
-    encrypt        = true
+    bucket       = "royal-hotel-tf-state-bucket"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
