03256b3f76a405f77e68f9a2fc36a8b8168d7b35
amankainth <aman.kainth@gmail.com>
2026-07-14 11:51:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,59 @@
-terraform/variables.tf
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+  default     = "royal-hotel"
+}
+
+variable "environment" {
+  description = "Environment name (dev/staging/prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.10.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "AWS Availability Zone"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "root_volume_size" {
+  description = "Root EBS volume size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed for SSH access"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "ssh_key_path" {
+  description = "Local path to save SSH private key (on Jenkins)"
+  type        = string
+  default     = "/var/lib/jenkins/.ssh/royal-hotel-dev-key.pem"
+}
